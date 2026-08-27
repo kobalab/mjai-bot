078b4d1003cf8501c0f9de27d71e17c59c846602
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-08-27 10:35:27 +0800

    通信内容をダンプする処理を追加(応答は常に type: 'none')

--- mjai-bot.js
+++ mjai-bot.js
@@ -7,4 +7,13 @@ const net = require('net');
 const host = process.argv[3]   || 'localhost';
 const port = + process.argv[2] || 11600;
 
-const sock = net.connect(port, host);
+const sock = net.connect(port, host, ()=>{
+    sock.on('data', (data)=>{
+        let msg = JSON.parse(data.toString('utf-8'));
+        console.log('<-', msg);
+
+        let reply = { type: 'none' };
+        console.log('->', reply);
+        sock.write(JSON.stringify(reply) + '\n');
+    });
+});
