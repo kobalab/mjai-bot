3cd15e7b70ece5b0e704f23af9732eab6f08d377
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-08-27 10:46:30 +0800

    対局終了まで正しい応答をするよう修正

--- mjai-bot.js
+++ mjai-bot.js
@@ -8,11 +8,31 @@ const host = process.argv[3]   || 'localhost';
 const port = + process.argv[2] || 11600;
 
 const sock = net.connect(port, host, ()=>{
+
+    let id;
+
     sock.on('data', (data)=>{
         let msg = JSON.parse(data.toString('utf-8'));
         console.log('<-', msg);
 
         let reply = { type: 'none' };
+
+        if (msg.type == 'hello') {
+            reply = { type: 'join', name: 'mjai-bot', room: 'default' };
+        }
+        else if (msg.type == 'start_game') {
+            id = msg.id;
+        }
+        else if (msg.type == 'tsumo') {
+            if (msg.actor == id) {
+                reply = {
+                    type: 'dahai',
+                    actor: id,
+                    pai: msg.pai,
+                    tsumogiri: true
+                };
+            }
+        }
         console.log('->', reply);
         sock.write(JSON.stringify(reply) + '\n');
     });
