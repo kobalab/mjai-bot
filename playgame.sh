5ac5561e2aa837a20fcc42bad1ac0a5beb943fa9
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-08-29 22:43:31 +0800

    playgame.sh がログファイルを保存しないバグを修正

--- playgame.sh
+++ playgame.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -e
 
-test $2 && logfile=$2 || logfile=./log/`date '+%Y%m%d-%H%M%S'`.json
-
 for i in $(seq $1 -1 1)
 do
+    test $2 && logfile=$2 || logfile=./log/`date '+%Y%m%d-%H%M%S'`.json
     node bin/mjai-diannao.js mjsonp://localhost:11600/default -v -o $logfile
     echo; echo "[$((i - 1))]" `date` >&2 ; echo
     sleep 1
