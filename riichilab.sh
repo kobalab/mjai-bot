05b285729fa7f4120b9e83a207bb5396bfc9ad4c
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-08-31 10:33:47 +0800

    RiichiLab用スクリプトを修正
      - 引数を指定可能にした
      - 無限ループできるようにした

--- riichilab.sh
+++ riichilab.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 set -e
 
-for i in $(seq $1 -1 1)
-do
-    node dev/riichilab.js -v
-    echo; echo "[$((i - 1))]" `date` >&2 ; echo
-    sleep 1
-done
+[ $# -gt 0 ] || { echo "Usage: $0 count params..."; exit 1; }
+
+n=$1; shift
+
+if [ $n = '-' ]
+then
+    while :
+    do
+        node dev/riichilab.js "$@"
+        date
+        sleep 60
+    done
+else
+    while [ $n -gt 0 ]
+    do
+        node dev/riichilab.js "$@"
+        n=$((n - 1))
+        echo "[$n] $(date)"
+        [ $n -gt 0 ] && sleep 60
+    done
+fi
