69da565ecbb67a0aa7577ce7e890ac306e50ebc3
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-08-31 19:58:07 +0800

    SIGTERM で安全に止まるよう修正

--- riichilab.sh
+++ riichilab.sh
@@ -3,22 +3,26 @@ set -e
 
 [ $# -gt 0 ] || { echo "Usage: $0 count params..."; exit 1; }
 
+trap '' HUP
+stop=0
+trap 'stop=1' TERM
+
 n=$1; shift
 
 if [ $n = '-' ]
 then
-    while :
+    while [ $stop -eq 0 ]
     do
         node dev/riichilab.js "$@"
-        date
+        [ $stop -eq 1 ] && exit
         sleep 60
     done
 else
-    while [ $n -gt 0 ]
+    while [ $n -gt 0 ] && [ $stop -eq 0 ]
     do
         node dev/riichilab.js "$@"
+        [ $stop -eq 1 ] && exit
         n=$((n - 1))
-        echo "[$n] $(date)"
         [ $n -gt 0 ] && sleep 60
     done
 fi
