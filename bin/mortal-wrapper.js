484ca2656d6e8e4ced17577428c425c789bec13b
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-09-17 06:14:10 +0800

    Akagiのボットを起動するオプション --akagi を追加

--- bin/mortal-wrapper.js
+++ bin/mortal-wrapper.js
@@ -11,6 +11,7 @@ const { spawn } = require('child_process');
 const argv = require('yargs')
     .usage('Usage: $0 mjsonp://<host>:<port>/<room> mortal-dir')
     .option('verbose', { alias: 'v', boolean: true })
+    .option('akagi',   {             boolean: true })
     .demandCommand(2)
     .argv;
 
@@ -21,13 +22,22 @@ if (! host) {
     process.exit(-1);
 }
 
-const workdir = path.join(path.resolve(argv._[1]), 'mortal');
+const workdir = argv.akagi ? path.resolve(argv._[1])
+                           : path.join(path.resolve(argv._[1]), 'mortal');
+
+const name = argv.akagi ? 'Akagi' : 'Mortal';
 
 function exec_mortal(id) {
     return spawn('uv', ['run','python','mortal.py', id],
                     { cwd:   workdir,
                       env:   {  ...process.env,
                                 MORTAL_REVIEW_MODE: 1 },
+                      stdio: ['pipe','pipe','inherit'] });
+}
+
+function exec_akagi() {
+    return spawn('uv', ['run','python','bot.py'],
+                    { cwd:   workdir,
                       stdio: ['pipe','pipe','inherit'] });
 }
 
@@ -41,23 +51,25 @@ const sock = net.connect(port, host, ()=>{
         if (msg.type == 'hello') {
             if (argv.verbose) console.log('<-', util.inspect(msg,
                                             { depth: null, colors: true }));
-            let rep = { type: 'join', name: 'Mortal', room: room };
+            let rep = { type: 'join', name: name, room: room };
             if (argv.verbose) console.log('->', util.inspect(rep,
                                                 { depth: null, colors: true }));
             sock.write(JSON.stringify(rep) + '\n');
             return;
         }
         else if (msg.type == 'start_game') {
-            if (argv.verbose) console.log('<-', util.inspect(msg,
-                                            { depth: null, colors: true }));
             id = msg.id;
-            bot = exec_mortal(id);
-            readline.createInterface(bot.stdout).on('line', fixrep);
-            let rep = { type: 'none' };
-            if (argv.verbose) console.log('->', util.inspect(rep,
+            if (! argv.akagi) {
+                if (argv.verbose) console.log('<-', util.inspect(msg,
                                                 { depth: null, colors: true }));
-            sock.write(JSON.stringify(rep) + '\n');
-            return;
+                bot = exec_mortal(id);
+                readline.createInterface(bot.stdout).on('line', fixrep);
+                let rep = { type: 'none' };
+                if (argv.verbose) console.log('->', util.inspect(rep,
+                                                { depth: null, colors: true }));
+                sock.write(JSON.stringify(rep) + '\n');
+                return;
+            }
         }
         else if (msg.type == 'start_kyoku') {
             if (! msg.scores) msg.scores = scores;
@@ -69,13 +81,14 @@ const sock = net.connect(port, host, ()=>{
 
         if (argv.verbose) console.log('<-', util.inspect(msg,
                                             { depth: null, colors: true }));
-        bot.stdin.write(JSON.stringify(msg) + '\n');
+        if (argv.akagi) bot.stdin.write(JSON.stringify([ msg ]) + '\n');
+        else            bot.stdin.write(JSON.stringify(msg) + '\n');
 
         if (msg.scores) scores = msg.scores;
         if (msg.pai)    pai    = msg.pai;
 
         if (msg.type == 'end_game') {
-            bot.kill('SIGINT');
+            if (! argv.akagi) bot.kill('SIGINT');
         }
     }
 
@@ -94,6 +107,11 @@ const sock = net.connect(port, host, ()=>{
         if (argv.verbose) console.log('->', util.inspect(rep,
                                             { depth: null, colors: true }));
         sock.write(JSON.stringify(rep) + '\n');
+    }
+
+    if (argv.akagi) {
+        bot = exec_akagi();
+        readline.createInterface(bot.stdout).on('line', fixrep);
     }
 
     readline.createInterface(sock).on('line', fixmsg);
